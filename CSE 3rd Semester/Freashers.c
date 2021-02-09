2edc115655616adecdd9a534c88a12e2d05ac3f3
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2021-02-10 01:58:16 +0600

    adding 3rd res 6

---
+++ CSE 3rd Semester/Freashers.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main () {
+
+   int n;
+   printf("Enter your choice:  ");
+   scanf("%d",&n);
+   printf("\n\n");
+   switch(n) {
+      case 1 :
+         printf("It's Blank!!! Thanks... Take your seat.\n\n\n\n\n\n" );
+         break;
+      case 2 :
+         printf("It's Blank!!! Thanks... Take your seat.\n\n\n\n\n\n");
+         break;
+      case 3 :
+         printf("WoW!!!... Dance!!!\n\n\n\n\n\n" );
+         break;
+      case 4 :
+         printf("It's Blank!!! Thanks... Take your seat.\n\n\n\n\n\n" );
+         break;
+      case 5 :
+         printf("Oh!!... It's Song..!!\n\n\n\n\n\n" );
+         break;
+      case 6 :
+         printf("You have to act as a 'Street Beggar' ...LOL!!!\n\n\n\n\n\n" );
+         break;
+      case 7 :
+         printf("It's Blank!!! Thanks... Take your seat.\n\n\n\n\n\n" );
+         break;
+      case 8 :
+         printf("It's Blank!!! Thanks... Take your seat.\n\n\n\n\n\n" );
+         break;
+      case 9 :
+         printf("It's Blank!!! Thanks... Take your seat.\n\n\n\n\n\n" );
+         break;
+      case 10 :
+         printf("Better try again\n" );
+         break;
+      case 11 :
+         printf("Excellent!\n" );
+         break;
+      case 12 :
+         printf("Well done\n" );
+         break;
+      case 13 :
+         printf("Well done\n" );
+         break;
+      case 14 :
+         printf("You passed\n" );
+         break;
+      case 15 :
+         printf("Better try again\n" );
+         break;
+      case 16 :
+         printf("Excellent!\n" );
+         break;
+      case 17 :
+         printf("Well done\n" );
+         break;
+      case 18 :
+         printf("Well done\n" );
+         break;
+      case 19 :
+         printf("You passed\n" );
+         break;
+      case 20 :
+         printf("Better try again\n" );
+         break;
+      case 21 :
+         printf("Excellent!\n" );
+         break;
+      case 22 :
+         printf("Well done\n" );
+         break;
+      case 23 :
+         printf("Well done\n" );
+         break;
+      case 24 :
+         printf("You passed\n" );
+         break;
+      case 25 :
+         printf("Better try again\n" );
+         break;
+      case 26 :
+         printf("Excellent!\n" );
+         break;
+      case 27 :
+         printf("Well done\n" );
+         break;
+      default :
+         printf("Invalid grade\n" );
+   }
+
+   return main();
+}
---
+++ CSE 3rd Semester/Lottery.cpp
@@ -0,0 +1,175 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+
+class A{
+public:
+    /*wlcm()
+    {
+        cout<<"Welcome..!!\n\n";
+    }*/
+    swch()
+    {
+        int n;
+   printf("Enter your choice:  ");
+   scanf("%d",&n);
+   printf("\n\n");
+   switch(n) {
+      case 1 :
+         printf("As you wish: Dance, Song, Acting, Comedy..\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 2 :
+         printf("Comedy...!!\n\n\n");
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 3 :
+         printf("WoW!!!... Dance!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 4 :
+         printf("As you wish: Dance, Song, Comedy, Acting..\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 5 :
+         printf("Oh!!... It's Song..!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 6 :
+         printf("You have to act as a 'Street Beggar' ...LOL!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 7 :
+         printf("Be a Comedian..\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 8 :
+         printf("As you wish: Dance, Song, Comedy, Acting..\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 9 :
+         printf("Be a Celebraty, Keep a Speech.\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 10 :
+         printf("Poetry Recitation...!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 11 :
+         printf("Yes..!! It's Dance..!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 12 :
+         printf("!!!...Comedy...!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 13 :
+         printf("Sing A Song...!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 14 :
+         printf("As you wish: Dance, Song, Comedy, Acting..\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 15 :
+         printf("Poetry Recitation...!!! WoW....\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 16 :
+         printf("Act Like a Old Man...!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 17 :
+         printf("OWW..!! Dance..!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 18 :
+         printf("LoL..!!!Comedy...!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 19 :
+         printf("As you wish: Dance, Song, Comedy, Acting..\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 20 :
+         printf("LoL..!!!Comedy...!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 21 :
+         printf("Poetry Recitation...!!! WoW....\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 22 :
+         printf("Hay..!! Dance....!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 23 :
+         printf("Sing A Song...!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 24 :
+         printf("WoW!!!... Dance!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 25 :
+         printf("Act like a 'Street Seller..!!!!\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 26 :
+         printf("Poetry Recitation...!!! WoW....\\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      case 27 :
+         printf("As you wish: Dance, Song, Comedy, Acting..\n\n\n" );
+         printf("Courtesy: 1st Batch, CSE\n\n\n\n");
+         break;
+      default :
+         printf("Unknown Number, Try Again..!!\n\n\n" );
+              }
+    }
+    limit()
+    {
+        cout<<"Limit Over. Try again...!\n\n\n";
+    }
+};
+main()
+{
+    A obj;
+    //obj.wlcm();
+
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+    obj.swch();
+
+    obj.limit();
+
+
+
+
+
+}
