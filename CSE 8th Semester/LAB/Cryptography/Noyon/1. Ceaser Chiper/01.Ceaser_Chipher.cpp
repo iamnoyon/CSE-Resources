75087d100ab4e7c397554e0ea07929407d664c17
Mahfujar Rahman Noyon <31146513+iamnoyon@users.noreply.github.com>
2023-05-31 00:18:54 +0600

    LAB part 2

---
+++ CSE 8th Semester/LAB/Cryptography/Noyon/1. Ceaser Chiper/01.Ceaser_Chipher.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define SIZE 1000000
+
+char str[SIZE];
+
+int encrypt_text(int ch){
+    ch = (ch + 3) % 26;
+}
+
+int decrypt_text(int ch){
+    ch = (ch - 3 + 26) % 26;
+}
+
+int main(){
+    cout<<"Enter the plain text : ";
+    gets(str);
+
+    for(int i = 0; str[i]; i++){
+        //if(isupper(str[i])){
+        //char p = str[i] - 'A';
+    cout<<str[i] - 'A'<<endl;
+            str[i] = (char)(encrypt_text((str[i] - 'A')) + 'A');
+        }
+        else if(islower(str[i])){
+            str[i] = (char)(encrypt_text((str[i] - 'a')) + 'a');
+        }
+    }
+
+    cout<<"Encrypted Text : "<<str<<endl;
+
+    for(int i = 0; str[i]; i++){
+        if(isupper(str[i])){
+            str[i] = (char)(decrypt_text((str[i] - 'A')) + 'A');
+        }
+        else if(islower(str[i])){
+            str[i] = (char)(decrypt_text((str[i] - 'a')) + 'a');
+        }
+    }
+
+    cout<<"Decripted Text : "<<str<<endl;
+
+    return 0;
+}
